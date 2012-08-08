1f1ba0ecf4f0f0950ffbf4a7eebc5f1d4cd4ea61
Philip A Senger <philip.a.senger@cngrgroup.com>
2012-08-07 23:53:19 -0400

    Refactored some classes to make it more readable and added some tests

--- src/com/cngrgroup/graphtools/NonCycleGraphTools.java
+++ src/com/cngrgroup/graphtools/NonCycleGraphTools.java
@@ -1,73 +1,9 @@
 package com.cngrgroup.graphtools;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Stack;
 
-public class GraphTools
+public class NonCycleGraphTools
 {
-    /**
-     * Depth first search
-     */
-    public static Node DFS(Node node, String target, Stack<String> path)
-    {
-        node.setVisited(true);
-        path.push(node.getCommit());
-        if (target.equals(node.getCommit()))
-        {
-            return node;
-        }
-        else
-        {
-            Node result = null;
-            for (Node child : node.getChildren())
-            {
-                if (!child.isVisited())
-                {
-                    result = DFS(child, target, path);
-                }
-                if (result != null)
-                {
-                    break;
-                }
-                else
-                {
-                    path.pop();
-                }
-            }
-            return result;
-        }
-    }
-
-    /**
-     * Breath First Search
-     */
-    public static Node BFS(Node node, String target, Stack<String> path)
-    {
-        LinkedList<Node> queue = new LinkedList<Node>();
-        queue.add(node);
-        node.setVisited(true);
-        while (!queue.isEmpty())
-        {
-            Node currentNode = queue.poll();
-
-            path.push(currentNode.getCommit());
-
-            if ((target.equals(node.getCommit())))
-            {
-                return currentNode;
-            }
-            for (Node child : currentNode.getChildren())
-            {
-                if (!child.isVisited())
-                {
-                    child.setVisited(true);
-                    queue.add(child);
-                }
-            }
-        }
-        return null;
-    }
 
     public static HashMap<String, Node> buildGraphWithChildren(String[] commits, String[][] children)
     {
@@ -129,22 +65,4 @@ public class GraphTools
         }
     }
 
-    public static String LCA(String[] path1, String[] path2)
-    {
-        String lca = null;
-        String[] smallestPath = (path1.length > path2.length) ? path2 : path1;
-        String[] largestPath = (path1.length > path2.length) ? path1 : path2;
-        for (int i = 0; i < smallestPath.length; i++)
-        {
-            if ((smallestPath[i].equals(largestPath[i])))
-            {
-                lca = smallestPath[i];
-            }
-            else
-            {
-                break;
-            }
-        }
-        return lca;
-    }
 }
---
+++ src/com/cngrgroup/graphtools/SearchTools.java
@@ -0,0 +1,106 @@
+package com.cngrgroup.graphtools;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class SearchTools
+{
+    /**
+     * Depth first search
+     *
+     * @param node   a non-null node
+     * @param target a non-null target
+     * @param path   resulting path.
+     * @return returns null for not found or the node.
+     */
+    public static Node DFS(Node node, String target, Stack<String> path)
+    {
+        node.setVisited(true);
+        path.push(node.getCommit());
+        if (target.equals(node.getCommit()))
+        {
+            return node;
+        }
+        else
+        {
+            Node result = null;
+            for (Node child : node.getChildren())
+            {
+                if (!child.isVisited())
+                {
+                    result = DFS(child, target, path);
+                }
+                if (result != null)
+                {
+                    break;
+                }
+                else
+                {
+                    path.pop();
+                }
+            }
+            return result;
+        }
+    }
+
+    /**
+     * Breath First Search
+     *
+     * @param node   a non-null node
+     * @param target a non-null target
+     * @param path   resulting path.
+     * @return returns null for not found or the node.
+     */
+    public static Node BFS(Node node, String target, Stack<String> path)
+    {
+        LinkedList<Node> queue = new LinkedList<Node>();
+        queue.add(node);
+        node.setVisited(true);
+        while (!queue.isEmpty())
+        {
+            Node currentNode = queue.poll();
+
+            path.push(currentNode.getCommit());
+
+            if ((target.equals(node.getCommit())))
+            {
+                return currentNode;
+            }
+            for (Node child : currentNode.getChildren())
+            {
+                if (!child.isVisited())
+                {
+                    child.setVisited(true);
+                    queue.add(child);
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Lowest Common Ansestor.
+     *
+     * @param path1
+     * @param path2
+     * @return
+     */
+    public static String LCA(String[] path1, String[] path2)
+    {
+        String lca = null;
+        String[] smallestPath = (path1.length > path2.length) ? path2 : path1;
+        String[] largestPath = (path1.length > path2.length) ? path1 : path2;
+        for (int i = 0; i < smallestPath.length; i++)
+        {
+            if ((smallestPath[i].equals(largestPath[i])))
+            {
+                lca = smallestPath[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        return lca;
+    }
+}
